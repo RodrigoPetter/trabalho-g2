8f6f822b1d4af0ff9e736ab5be55a3b90fb2f17c
Rodrigo Petter <guigo45@gmail.com>
2018-05-06 21:13:54 -0300

    Add datatable

--- client/src/components/concurso/concursos.js
+++ client/src/components/concurso/concursos.js
@@ -1,11 +1,76 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
+import ReactTable from "react-table";
+
+const data = [{
+    name: 'Tanner Linsley',
+    age: 26,
+    friend: {
+        name: 'Jason Maurer',
+        age: 23,
+    }
+},{
+    name: 'Tanner Linsley',
+    age: 26,
+    friend: {
+        name: 'Jason Maurer',
+        age: 23,
+    }
+},{
+    name: 'Tanner Linsley',
+    age: 26,
+    friend: {
+        name: 'Jason Maurer',
+        age: 23,
+    }
+},{
+    name: 'Tanner Linsley',
+    age: 26,
+    friend: {
+        name: 'Jason Maurer',
+        age: 23,
+    }
+},{
+    name: 'Rodrigo',
+    age: 26,
+    friend: {
+        name: 'aaaaaaaa',
+        age: 23,
+    }
+}];
+
+const columns = [{
+    Header: 'Name',
+    accessor: 'name' // String-based value accessors!
+}, {
+    Header: 'Age',
+    accessor: 'age',
+    Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
+}, {
+    id: 'friendName', // Required because our accessor is not a string
+    Header: 'Friend Name',
+    accessor: d => d.friend.name // Custom value accessors!
+}, {
+    Header: props => <span>Friend Age</span>, // Custom header components!
+    accessor: 'friend.age'
+}];
 
 class Concursos extends Component {
     render() {
         return (
-            <div>
-                <h2>Concursos</h2>
-
+            <div className="container bg-white">
+                <h3 className="border-bottom border-gray pb-2 mb-0">Concursos</h3>
+                <div className="row margin15">
+                    <div className="col">
+                        <button type="button" className="btn btn-primary btn-sm">Adicionar</button>
+                    </div>
+                </div>
+                <div className="row margin15">
+                    <ReactTable
+                        data={data}
+                        columns={columns}
+                        filterable
+                    />
+                </div>
             </div>
         );
     }
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import './custom.css';
+import "react-table/react-table.css";
+
 //Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
